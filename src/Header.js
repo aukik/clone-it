1b1e5e7060e0159023bf24f20648d952450734f2
Aukik Aurnab <55758994+aukik@users.noreply.github.com>
2022-09-16 09:15:31 +0600

    gg

--- src/Header.js
+++ src/Header.js
@@ -4,19 +4,22 @@ import PersonIcon from "@mui/icons-material/Person"
 import Button from "react-bootstrap/Button"
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import IconButton from "@mui/material/IconButton"
+import ForumIcon from "@mui/icons-material/Forum"
+
 function Header() {
   return (
     <div className='header'>
-      <div>
-        <IconButton className='icon_button'>
-          <PersonIcon fontSize='large' className='Person' />
-        </IconButton>
+      <IconButton className='icon_button'>
+        <PersonIcon fontSize='large' className='Person' />
+      </IconButton>
 
-        <img
-          className='header_logo'
-          src='https://wallpaperaccess.com/full/322057.png'
-        />
-      </div>
+      <img
+        className='header_logo'
+        src='https://wallpaperaccess.com/full/322057.png'
+      />
+      <IconButton className='forum_button'>
+        <ForumIcon />
+      </IconButton>
     </div>
   )
 }
