205ea06058acecb1ee173568c8a56cd4b5bc130c
Aukik Aurnab <55758994+aukik@users.noreply.github.com>
2022-09-16 08:47:20 +0600

    gg

---
+++ src/Header.js
@@ -0,0 +1,26 @@
+import React from "react"
+import "./Header.css"
+import PersonIcon from "@mui/icons-material/Person"
+import Button from "react-bootstrap/Button"
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import IconButton from "@mui/material/IconButton"
+function Header() {
+  return (
+    <div className='header'>
+      <div>
+        <IconButton className='icon_button'>
+          <PersonIcon fontSize='large' className='Person' />
+        </IconButton>
+
+        <img
+          className='header_logo'
+          src='https://wallpaperaccess.com/full/322057.png'
+        />
+      </div>
+
+      {/* <Button variant='primary'>Primary</Button> */}
+    </div>
+  )
+}
+
+export default Header
---
+++ src/hlink.js
@@ -0,0 +1,34 @@
+import React from "react"
+
+function hlink() {
+  return (
+    <div>
+      <link
+        rel='stylesheet'
+        href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
+      />
+      <link
+        rel='stylesheet'
+        href='https://fonts.googleapis.com/icon?family=Material+Icons'
+      />
+      <script
+        src='https://unpkg.com/react/umd/react.production.min.js'
+        crossorigin
+      />
+
+      <script
+        src='https://unpkg.com/react-dom/umd/react-dom.production.min.js'
+        crossorigin
+      />
+
+      <script
+        src='https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js'
+        crossorigin
+      />
+
+      <script>var Alert = ReactBootstrap.Alert;</script>
+    </div>
+  )
+}
+
+export default hlink
